42180310154ce2b9b6ed091346c6a433a65ba162
RohanK-coder <kyrohan7730@gmail.com>
2024-11-15 12:06:49 +0530

    errors recified and logged

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURL = 'mongodb+srv://{username}:{password}@banking.gl2qj.mongodb.net/?retryWrites=true&w=majority&appName=banking'
+export const mongoDBURL = 'mongodb+srv://root:root@banking.gl2qj.mongodb.net/?retryWrites=true&w=majority&appName=banking'
